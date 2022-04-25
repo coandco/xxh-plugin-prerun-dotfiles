55d9a5b565c8240c4b7dee3143a58ecc5527b0f0
Clint Olson <clint@populi.co>
2022-04-25 11:48:37 -0700

    Add the ability to install portable tools to .local/bin

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 build_dir=$CDIR/build
 
@@ -35,3 +34,34 @@ if [ -x "$(command -v pip)" ]; then
 else
   echo 'Skip pip packages installation: pip not found.'
 fi
+
+for f in *-tool.sh; do
+  required_vars=(tool_github_repo tool_arch install_tool)
+  for required_var in "${required_vars[@]}"; do
+    unset "$required_var"
+  done
+
+  tool_name="${f%-tool.sh}"
+  echo "Attempting to install $tool_name..."
+
+  # Import tool definition and verify that it defined the correct vars
+  source "$f"
+  for required_var in "${required_vars[@]}"; do
+    if [ "$(type -t "$required_var")" == "function" ] || [ -n "${!required_var}" ]; then
+      continue
+    fi
+    echo "Required var $required_var not found in file $f"
+    exit 1
+  done
+
+  release_url="https://api.github.com/repos/${tool_github_repo}/releases/latest"
+  regex_before='https.*'
+  regex_after='[^"]*'
+  download_url=`curl -s "$release_url" | grep "browser_download_url" | grep -wo "${regex_before}${tool_arch}${regex_after}" | head -n 1`
+  output_filename=`basename "$download_url"`
+  echo "Downloading $download_url to $build_dir/$output_filename..."
+  curl -Ls "$download_url" -o "$build_dir/$output_filename"
+  mkdir -p "$build_dir/home/.local/bin"
+  echo "Installing $tool_name..."
+  install_tool "$build_dir/$output_filename" "$build_dir/home/.local/bin"
+done
