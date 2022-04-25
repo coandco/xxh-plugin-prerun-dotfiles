dda4ce4e09e7b367c6e568d1390a82f0bb663bba
Clint Olson <clint@populi.co>
2022-04-25 15:51:32 -0700

    Add diff-so-fancy to the list of tools

---
+++ diff-so-fancy-tool.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+tool_github_repo="so-fancy/diff-so-fancy"
+tool_arch="diff-so-fancy"
+
+install_tool() {
+  local downloaded_file="$1"
+  local output_dir="$2"
+  if [ -f "$downloaded_file" ]; then
+    chmod a+x "$downloaded_file"
+    mv "$downloaded_file" "$output_dir/"
+  else
+    echo "Couldn't find downloaded diff-so-fancy at $downloaded_file!"
+    exit 1
+  fi
+}
