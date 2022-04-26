d73233331117b220ee36f6151684c53ca8022169
Clint Olson <clint@populi.co>
2022-04-26 08:38:07 -0700

    AppImage extract is noisy, so suppress its output

--- 110-setup-xonsh-prerun.sh
+++ 110-setup-xonsh-prerun.sh
@@ -2,7 +2,8 @@
 
 if [ ! -d ~/.local/bin/xonsh-files ] && [ -x ~/.local/bin/xonsh-appimage ]; then
   cd ~/.local/bin
-  ./xonsh-appimage --appimage-extract
+  # This is noisy, so we want to suppress the output
+  ./xonsh-appimage --appimage-extract > /dev/null
   mv squashfs-root xonsh-files
   ln -s xonsh-files/usr/bin/xonsh xonsh
   ln -s xonsh-files/usr/bin/python xonsh-python
